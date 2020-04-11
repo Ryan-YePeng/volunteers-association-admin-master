91a3d25d7ef25cc3a7bb09790867eebd59d76a85
JialeHub <799670335@qq.com>
2020-04-11 17:17:23 +0800

    activity

--- src/api/activity.js
+++ src/api/activity.js
@@ -1,16 +1,22 @@
 import {axiosD, axiosG, axiosK, axiosP} from "./request";
 
-const addActivityUrl = "article/addActivity"; // 添加活动
-const addActivityApplyUrl = "article/addActivityApply"; // 添加活动报名列表
-const delActivityUrl = "article/delActivity?id="; // 删除活动
-const editActivityUrl = "article/editActivity"; // 编辑活动
-const getArticleUrl = "article/getArticle?id="; // 获得活动详情
-const pageActivityUrl = "article/pageActivity?"; //分页获得活动列表
+const activityApplyCheckUrl = "activity/activityApplyCheck"; // 审核活动报名
+const addActivityUrl = "activity/addActivity"; // 添加活动
+const addActivityApplyUrl = "activity/addActivityApply"; // 添加活动报名列表
+const delActivityUrl = "activity/delActivity?id="; // 删除活动
+const delApplyCheckUrl = "activity/delApplyCheck?ids="; // 删除活动报名
+const editActivityUrl = "activity/editActivity"; // 编辑活动
+const getArticleUrl = "activity/getArticle?id="; // 获得活动详情
+const pageActivityUrl = "activity/pageActivity?"; //分页获得活动列表
+const pageActivityApplyUrl = "activity/pageActivityApply?"; //分页获得活动列表
 
+export const activityApplyCheckApi = data => axiosP(activityApplyCheckUrl, data);
 export const addActivityApi = data => axiosK(addActivityUrl, data);
 export const addActivityApplyApi = data => axiosK(addActivityApplyUrl, data);
 export const delActivityApi = data => axiosD(delActivityUrl + data);
+export const delApplyCheckApi = data => axiosD(delApplyCheckUrl + data);
 export const editActivityApi = data => axiosP(editActivityUrl, data);
+export const getArticleApi = data => axiosG(getArticleUrl + data);
 export const pageActivityApi = data => axiosG(pageActivityUrl + data);
-
+export const pageActivityApplyApi = data => axiosG(pageActivityApplyUrl + data);
 
