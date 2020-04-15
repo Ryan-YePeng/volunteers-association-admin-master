4450e4310c1820b48ed229c48fc1a4d763dfda84
Administrator <2912240796@qq.com>
2020-04-15 15:48:18 +0800

    20200415

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,7 @@
 </template>
 
 <script>
-  import {axiosGs} from '@/api/request/index'
-
   export default {
-    name: "App",
-    mounted() {
-      const getUserListUrl = "admin/user/pageUser"; // 获得用户列表
-      let data = {
-        current: 1,
-        size: 10,
-        username: ''
-      };
-      axiosGs(getUserListUrl, data)
-    }
+    name: "App"
   };
 </script>
